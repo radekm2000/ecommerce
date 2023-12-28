99690083f6f0f6678da3fa4d39459e64811273e1
Radek <104318242+radekm2000@users.noreply.github.com>
2023-12-28 16:41:58 +0100

    add zodExceptionFilter and create users module

--- server/ecommerce/src/main.ts
+++ server/ecommerce/src/main.ts
@@ -1,8 +1,12 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ZodExceptionFilter } from './utils/ZodExceptionFilter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new ZodExceptionFilter(httpAdapter));
   await app.listen(3000);
 }
 bootstrap();
---
+++ server/ecommerce/src/utils/ZodExceptionFilter.ts
@@ -0,0 +1,24 @@
+import { ArgumentsHost, Catch, HttpStatus } from '@nestjs/common';
+import { ZodError } from 'zod';
+import { Response } from 'express';
+import { BaseExceptionFilter } from '@nestjs/core';
+
+@Catch()
+export class ZodExceptionFilter extends BaseExceptionFilter {
+  public catch(exception: any, host: ArgumentsHost) {
+    if (!this.isZodError(exception)) {
+      return super.catch(exception, host);
+    }
+
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    return response.status(HttpStatus.UNPROCESSABLE_ENTITY).json({
+      type: 'validation',
+      message: exception.issues[0]?.message || 'Unknown error',
+    });
+  }
+
+  private isZodError = (err: any): err is ZodError => {
+    return err.name === 'ZodError';
+  };
+}

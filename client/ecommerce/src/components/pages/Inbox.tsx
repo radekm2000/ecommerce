90ebd86225af1883f233c57343e59aac909d76ad
Radek <104318242+radekm2000@users.noreply.github.com>
2024-01-20 13:11:14 +0100

    create inbox layout

--- client/ecommerce/src/components/pages/Inbox.tsx
+++ client/ecommerce/src/components/pages/Inbox.tsx
@@ -1,6 +1,31 @@
+import { Box, Container, Typography } from "@mui/material";
+import { useMediaQuery } from "../../hooks/useMediaQuery";
 
 export const Inbox = () => {
+  const below1200 = useMediaQuery(1200);
+  const below960 = useMediaQuery(960);
+  const below1600 = useMediaQuery(1600);
   return (
-    <div>Inbox</div>
-  )
-}
+    <Container
+      maxWidth={false}
+      sx={{ maxWidth: "1280px", margin: below1600 ? null : "0px 150px", display: 'flex', padding: '20px 20px' }}
+    >
+      <Box
+        sx={{ width: below1200 ? "100%" : "75%", border: "1px solid black", padding: '0px 10px', display: 'flex' }}
+      >
+        {below960 ? null : (
+          <Box sx={{ flex: "0 0 300px", border: "1px solid black" }}>
+            <Typography>sidebar content</Typography>
+          </Box>
+        )}
+        <Box sx={{ flex: "1 0 auto", border: "1px solid black" }}>
+          <Typography>chat content</Typography>
+        </Box>
+      </Box>
+      <Box
+        sx={{ width: below1200 ? "none" : "25%", border: "1px solid black" }}
+      >
+      </Box>
+    </Container>
+  );
+};

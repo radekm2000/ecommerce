cbcbc4f0f5711f73da7c783bf1489e188c91a94c
Radek <104318242+radekm2000@users.noreply.github.com>
2024-02-11 18:04:48 +0100

    fix TypeError in nest by using setQueryData instead of invalidateQueries

--- client/ecommerce/src/hooks/useDeleteProduct.tsx
+++ client/ecommerce/src/hooks/useDeleteProduct.tsx
@@ -2,6 +2,7 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { deleteProduct } from "../api/axios";
 import toast from "react-hot-toast";
 import { useLocation } from "wouter";
+import { ProductWithImageAndUser } from "../types/types";
 
 export const useDeleteProduct = (productId: number | undefined) => {
   const [, setLocation] = useLocation();
@@ -11,8 +12,19 @@ export const useDeleteProduct = (productId: number | undefined) => {
     mutationKey: ["product", productId, "delete"],
     onSuccess: () => {
       toast.success("Product deleted");
-      queryClient.invalidateQueries({ queryKey: ["products"] });
       setLocation("/");
+      queryClient.setQueryData(
+        ["products"],
+        (oldData: ProductWithImageAndUser[]) => {
+          if (!oldData) return null;
+
+          const updatedData = oldData.filter(
+            (product) => product.id !== productId
+          );
+
+          return updatedData;
+        }
+      );
     },
     onError: (err) => {
       console.log(err);
